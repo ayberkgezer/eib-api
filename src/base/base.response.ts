2a1e90772d04a419def4a423e9ee223d8e6d241c
Ayberk Gezer <43863521+ayberkgezer@users.noreply.github.com>
2024-11-28 23:52:42 +0300

    initial commit

---
+++ src/base/base.response.ts
@@ -0,0 +1,10 @@
+export class BaseResponse<T> {
+    data: T;
+    message: string;
+    success: boolean;
+    constructor(data: T, message: string, success: boolean) {
+        this.data = data;
+        this.message = message;
+        this.success = success;
+    }
+}
