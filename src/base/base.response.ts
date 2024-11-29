ed13a2f5cd106530d44476daba513f69d2498f0c
Ayberk Gezer <43863521+ayberkgezer@users.noreply.github.com>
2024-11-30 01:54:16 +0300

    base-response

--- src/base/base.response.ts
+++ src/base/base.response.ts
@@ -2,9 +2,11 @@ export class BaseResponse<T> {
     data: T;
     message: string;
     success: boolean;
-    constructor(data: T, message: string, success: boolean) {
+    code: number;
+    constructor(data: T, message: string, success: boolean, code: number) {
         this.data = data;
         this.message = message;
         this.success = success;
+        this.code = code;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { ValidationPipe, BadRequestException, ValidationError, Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose']
+  });
+
+  const logger = new Logger('Validation');
+
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors: ValidationError[]) => {
+      const findFirstError = (errors: ValidationError[]) => {
+        for (const error of errors) {
+          if (error.constraints) {
+            logger.error(`Validation failed: ${JSON.stringify(error.constraints)}`);
+            return Object.values(error.constraints)[0];
+          }
+        }
+      }
+      const firstError = findFirstError(errors);
+      logger.error(`Bad Request: ${firstError}`);
+      return new BadRequestException(firstError);
+    },
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('NestJS EIB API')
+    .setDescription('The EIB AI API description')
+    .setVersion('0.1')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
